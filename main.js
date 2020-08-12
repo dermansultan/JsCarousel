19afbd6da6926a015972aa922adf7eab912365a8
dermansultan <abdurrehmansultan4@gmail.com>
2020-08-12 01:06:54 -0400

    Circle Render and CurrentIndex updated

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 const leftArrow = document.getElementById('leftArrow');
 const rightArrow = document.getElementById('rightArrow');
+const circleContainer = document.getElementById('circlesContainer');
 
 let slideList = document.querySelectorAll('.imgSlide');
 let currentSlideIndex = 0;
+
+const renderCircleIcon = function () {
+    let circleBtn = document.createElement('button');
+    circleBtn.className = 'circleBtn';
+    let circleBtnIcon = document.createElement('i');
+    circleBtnIcon.className = 'fas fa-circle';
+    circleBtn.appendChild(circleBtnIcon);
+    circleContainer.appendChild(circleBtn);
+}
+const renderCircleList = function () {
+    for (let i = 0; i < slideList.length; i++){
+        renderCircleIcon();
+    }
+}
+
+
+renderCircleList();
+
+let circleList = document.querySelectorAll('.circleBtn');
+console.log(circleList);
+
+
 console.log(currentSlideIndex);
 console.log(slideList);
 console.log(slideList[currentSlideIndex]);
+
 
 
 const setCurrentSlide = function (index) {
@@ -38,22 +62,40 @@ const renderCurrentSlide = function (index){
             value.style.display = 'none';
         }
 
-    })
+    });
     // slideList[index].classList.toggle('imgSlideNone');
 }
 
-// const getSlideList = function () {
-//     let slideList = document.querySelectorAll('.imgSlide');
-//     console.log(slideList);
-//     return slideList
-// }
+
+
+const renderCurrentCircle = function (index){
+    // Using the Index Display that one, Hide the Rest;
+    console.log(index);
+    console.log(circleList[index]);
+    circleList.forEach(function (value, index){
+        let circleIco = value.querySelector('.fa-circle');
+        if (index == currentSlideIndex){
+            circleIco.style.color = 'red';
+        } else {
+            circleIco.style.color = 'white';
+        }
+
+    });
+    // slideList[index].classList.toggle('imgSlideNone');
+}
+
+renderCurrentCircle(currentSlideIndex);
+
 
 leftArrow.addEventListener('click', () => {
     setCurrentSlide(-1);
     renderCurrentSlide(currentSlideIndex);
+    renderCurrentCircle(currentSlideIndex);
 });
 
 rightArrow.addEventListener('click', () => {
    setCurrentSlide(1);
    renderCurrentSlide(currentSlideIndex);
+   renderCurrentCircle(currentSlideIndex);
 });
+
